c1127b770a6c855952d5e005500b736230c1cff2
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-07-17 20:27:11 +0530

    Revert "Added:Daily Employee Wages Added"
    This reverts commit d35af7c9f4bfb9ccd48198f3acc612f641bab021.

--- src/employeewage.java
+++ src/employeewage.java
@@ -3,17 +3,11 @@ public class employeewage {
 	public static void main(String[] args)
 
 	{
-        int IS_FULL_TIME = 1 ;
-        double empCheck = Math.floor(Math.random() * 10) % 2 ;
-
-        if (empCheck == IS_FULL_TIME)
-           System.out.println("Employee is present ");
-        else
-           System.out.println("Employee is absent");
-
+		int IS_FULL_TIME = 1;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
 
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
