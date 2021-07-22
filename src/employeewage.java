138a97e21c1b9248d7a87428efe085554b0ba924
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-07-22 23:08:43 +0530

    Computing Wages for multiple companies

--- src/employeewage.java
+++ src/employeewage.java
@@ -1,36 +1,39 @@
 
 public class employeewage {
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	private static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MNTH = 10;
+	public static final int IS_PART_TIME = 0;
+	public static final int IS_FULL_TIME = 1;
 
-	public static int computeEmpWage()
+	public static int computeEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth)
 
 	{
 		System.out.println("Welcome to employee wage computation!");
-
+		//While Condition To Calculate Total EmpWage
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
-		while (totalEmpHrs <= MAX_HRS_IN_MNTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
 
-			
-		totalEmpHrs += empHrs;
-		System.out.println("Day# " + totalWorkingDays + "Emp Hr: " + empHrs);
-		System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " + empHrs);
+				totalEmpHrs += empHrs;
+				System.out.println("Day# " + totalWorkingDays + "Emp Hr: " + empHrs);
+			}
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHr;
+		System.out.println("Total Emp Wage For Company: " + company + " is : " + totalEmpWage);
+		return totalEmpWage;
 	}
 
-	int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-	System.out.println("Total Emp Wage: "+totalEmpWage);return totalEmpWage;
-
-	}
+	/*
+	 * Declaring Main Method Calling ComputeEmpWage Method Here
+	 */
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("Dmart", 20, 20, 50);
+		computeEmpWage("Relaince", 30, 40, 70);
 	}
 
 }
